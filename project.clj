bd535f3cf15e2796c0254b553989de3179c92689
Karlis Lauva <k@karlis.me>
2017-08-28 10:33:35 -0600

    Version 2.2.2-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject fullcontact/http-kit "2.2.1"
+(defproject fullcontact/http-kit "2.2.2-SNAPSHOT"
   :author "Feng Shen (@shenfeng)"
   :description "High-performance event-driven HTTP client/server for Clojure"
   :url "http://http-kit.org/"
