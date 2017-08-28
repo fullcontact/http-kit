da84abc03b8c3a64660a6d85adfd9e2d6dd8417b
Karlis Lauva <k@karlis.me>
2017-08-28 10:30:19 -0600

    prepare for FC fork release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject http-kit "2.2.0"
+(defproject fullcontact/http-kit "2.2.1-SNAPSHOT"
   :author "Feng Shen (@shenfeng)"
   :description "High-performance event-driven HTTP client/server for Clojure"
   :url "http://http-kit.org/"
